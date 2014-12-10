e0ae31b9c58a353c284e1393d4baf30bb56e32ae
trikitrok <trikitrok@gmail.com>
2014-12-10 11:56:30 +0100

    Made helpers private

--- src/marvel_kata/comics/marvel_api/repository.clj
+++ src/marvel_kata/comics/marvel_api/repository.clj
@@ -4,20 +4,22 @@
             [marvel-kata.comics.marvel-api.config :as api-config]
             [marvel-kata.comics.marvel-api.comics-mapping :as mapping]))
 
-(declare get-as-json-map)
+(declare 
+  get-as-json-map
+  comics-url)
 
 (defn all-comics-for-date [timestamp]
   (mapping/to-comics
     (get-as-json-map timestamp)))
 
-(defn comics-url [timestamp]
+(defn- comics-url [timestamp]
   (api-url/generate 
     api-config/comics-api-url
     timestamp 
     api-config/public-key 
     api-config/private-key))
 
-(defn get-as-json-map [timestamp]
+(defn- get-as-json-map [timestamp]
   (client/get 
     (comics-url timestamp)
     {:as :json}))
